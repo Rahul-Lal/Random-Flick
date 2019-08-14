a53d198ad71918ffcc82558bd98ca05a75a6ee8b
Rahul Lal <Rahul21Lal@gmail.com>
2019-08-14 22:38:46 +1200

    Currently doing debugging

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import api from '../../server/api'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  state = {
+    film: ''
+  }
+
+  componentDidMount () {
+    api.getFilm()
+      .then(newFilm => {
+        this.setState({
+          film: newFilm
+        })
+      })
+  }
+
+  handleClick = () => {
+    const film = this.state.film
+
+    this.setState({ film })
+  }
+
+  render() {
+    const { film } = this.state
+     
+    return (
+      <React.Fragment>
+        <h1>I want to watch...</h1>
+
+        <button onClick={this.handleClick}>Choose</button>
+
+        {film && (<h2>film.original_title</h2>)}
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
