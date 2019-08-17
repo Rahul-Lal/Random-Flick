effcbcfe9ceda643aaf5a23707c52d8fed9c8fdb
Rahul Lal <Rahul21Lal@gmail.com>
2019-08-17 13:51:57 +1200

    API connection is working with selected data is s

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,35 +3,40 @@ import { getFilm } from '../api'
 
 class App extends React.Component {
   state = {
-    film: ''
+    title: '',
+    overview: '',
+    poster_path: ''
   }
 
   componentDidMount() {
     getFilm()
       .then(res => {
         this.setState({
-          film: res.body.title
+          title: res.body.title,
+          overview: res.body.overview,
+          poster_path: res.body.poster_path
         })
       })
-      .catch(err => this.setState({ film: 'https://i.gifer.com/XyGe.gif' }))
+      .catch(err => this.setState({ title: err.message }))
   }
 
   handleClick = () => {
-    const film = this.state.film
+    const title = this.state.title
+    const overview = this.state.overview
+    const poster_path = this.state.poster_path
 
-    this.setState({ film })
+    this.setState({ title, overview, poster_path })
   }
 
   render() {
     const { film } = this.state
-
     return (
       <React.Fragment>
         <h1>I want to watch...</h1>
 
         <button onClick={this.handleClick}>Choose</button>
 
-        <h2>{film.title}</h2>
+        {film && (<h2>film.title</h2>) && (<p>film.overview</p>) && (<img src={film.poster_path} />)}
       </React.Fragment>
     )
   }
