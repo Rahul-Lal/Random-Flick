5155228f8dbc0f7209b0b0b6bc99bdb98b582cf5
Rahul Lal <Rahul21Lal@gmail.com>
2019-08-17 21:10:37 +1200

    Added some error handling. refactor the console.logs, and working on getting UI to work

--- client/api.js
+++ client/api.js
@@ -1,4 +1,5 @@
 import request from 'superagent'
+let color = 'colors'
 
 /*
     Original link with fixed filmID number '707': 'A View To A Kill':
@@ -13,11 +14,11 @@ export function getFilm() {
         .then(res => {
             const title = res.body.title
             const overview = res.body.overview
-            const poster_path = res.body.poster_path
+            const poster_path = poster_source + res.body.poster_path
 
             console.log('Title: ' + title)
             console.log('Overview: ' + overview)
-            console.log('Poster: ' + poster_source + poster_path)
+            console.log('Poster: ' + poster_path)
             return title, overview, poster_path
         })
         .catch(err => {
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,15 +9,20 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    getFilm()
-      .then(res => {
-        this.setState({
-          title: res.body.title,
-          overview: res.body.overview,
-          poster_path: res.body.poster_path
+    try {
+      getFilm()
+        .then(res => {
+          this.setState({
+            title: res.body.title,
+            overview: res.body.overview,
+            poster_path: res.body.poster_path
+          })
         })
-      })
-      .catch(err => this.setState({ title: err.message }))
+        .catch(err => this.setState({ title: err.message }))
+    }
+    catch (error) {
+      console.log('https://thumbs.gfycat.com/RequiredExcellentDarwinsfox-size_restricted.gif\nERROR: ' + error.message)
+    }
   }
 
   handleClick = () => {
@@ -29,16 +34,23 @@ class App extends React.Component {
   }
 
   render() {
-    const { film } = this.state
-    return (
-      <React.Fragment>
-        <h1>I want to watch...</h1>
+    const film = this.state
+    try {
+      return (
+        <React.Fragment>
+          <h1>I want to watch...</h1>
 
-        <button onClick={this.handleClick}>Choose</button>
+          <button onClick={this.handleClick}>Choose</button>
 
-        {film && (<h2>film.title</h2>) && (<p>film.overview</p>) && (<img src={film.poster_path} />)}
-      </React.Fragment>
-    )
+          <h2>{film.title}</h2>
+          <p>{film.overview}</p>
+          <img src={film.poster_path} />
+        </React.Fragment>
+      )
+    }
+    catch (error) {
+      console.log('https://media0.giphy.com/media/BmnbtcKKBGqfS/source.gif\nERROR: ' + error.message)
+    }
   }
 }
 
